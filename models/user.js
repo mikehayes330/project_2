9625021fd8966ab777bde4f736c8c685ab836ff3
Mike Hayes <mikehayes330@gmail.com>
2020-08-29 14:06:48 -0400

    added name field to login form and created model for bookmarks

--- models/user.js
+++ models/user.js
@@ -16,8 +16,22 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
     }
   });
+
+  User.associate = function(models) {
+    User.hasMany(models.Bookmark, {
+      onDelete: "cascade"
+    });
+  };
+
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
